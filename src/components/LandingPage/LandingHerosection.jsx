8a6cda618348e6e40ffe68b9c1791d2602dd9a1d
developwithayush <developwithdubey@gmail.com>
2024-09-08 22:43:42 +0530

    fix some bugs

--- src/components/LandingPage/LandingHerosection.jsx
+++ src/components/LandingPage/LandingHerosection.jsx
@@ -11,8 +11,8 @@ const LandingHerosection = () => {
 
 
   return (
-    <section className='container relative mx-auto max-w-[88rem] h-[100vh] flex  flex-col items-start justify-center gap-[50px] px-[10px] '>
-          <h2 className='max-w-[444px] leading-[120%] text-[56px] font-mon_bold text-black '><span className='text-[#FF541E]'>Connect </span> with your circle in fun way !</h2>
+    <section className='container relative mx-auto max-w-[88rem] h-[100vh] flex  flex-col items-center lg:items-start justify-center gap-[50px] px-6 '>
+          <h2 className='max-w-[444px] leading-[120%] text-[56px]  lg:text-start text-center font-mon_bold text-black '><span className='text-[#FF541E]'>Connect </span> with your circle in fun way !</h2>
           <button onClick={(e) => {
               e.preventDefault();
               setOpenLogin(true)
@@ -24,7 +24,7 @@ const LandingHerosection = () => {
           <Fire top="15%" right="30%"/>
           <Fire top="50%" right="0%"/>
           <Heart top="70%" right="70%"/>
-          <Heart top="50%" right="20%"/>
+          <Heart top="60%" right="10%"/>
           <Heart top="90%" right="20%"/>
           <Heart top="70%" right="40%"/>
     </section>
@@ -34,9 +34,9 @@ const LandingHerosection = () => {
 
 const Male = () => {
     return (
-        <div className='z-10 absolute top-[40%] -translate-x-[70%] right-0 flex flex-row  items-center justify-center'>
-            <img src={male} alt="male" /> 
-            <div className='flex py-6 px-5 justify-center items-center rounded-t-[20px] rounded-br-[20px] bg-white text-[24px] font-mon_bold  '
+        <div className='z-10 absolute top-[80%] hidden  lg:top-[40%] -translate-x-[80%] lg:-translate-x-[70%] right-0 lg:flex flex-row  items-center justify-center'>
+            <img src={male} alt="male" className='w-[100px] lg:w-[200px]' /> 
+            <div className='flex p-3 lg:py-6  lg:px-5 justify-center items-center rounded-t-[20px] rounded-br-[20px] bg-white text-[16px] lg:text-[24px] font-mon_bold  '
                 style={{
                     boxShadow:" 4px 4px 83.4px 0px rgba(0, 0, 0, 0.30)"
             }}>Sure, see you soon!</div>
@@ -45,12 +45,12 @@ const Male = () => {
 }
 const Female = ({content, top,right}) => {
     return (
-        <div className='z-10 absolute   flex flex-row-reverse  items-center justify-center' style={{
+        <div className='z-10 absolute  hidden lg:flex flex-row-reverse gap-3 lg:gap-0  items-center justify-center' style={{
             top: top,
             right:right
         }}>
-            <img src={female} alt="male" /> 
-            <div className='flex py-6 px-5 justify-center items-center rounded-t-[20px] rounded-bl-[20px] text-white bg-[#FF541E] text-[24px] font-mon_bold  '
+            <img src={female} alt="male" className='w-[100px] lg:w-[200px]' /> 
+            <div className='flex  p-3 lg:py-6  lg:px-5 justify-center items-center rounded-t-[20px] rounded-bl-[20px] text-white bg-[#FF541E] text-[16px] lg:text-[24px] font-mon_bold  '
                 style={{
                     boxShadow:" 4px 4px 83.4px 0px rgba(0, 0, 0, 0.30)"
                 }}>{ content}</div>
