539e81b1a294b682e5c4c8c27dfd032ebe71e4ad
developwithayush <developwithdubey@gmail.com>
2024-09-08 22:12:04 +0530

    fix some bugs

--- src/components/shared/ChatCard.jsx
+++ src/components/shared/ChatCard.jsx
@@ -11,14 +11,14 @@ const ChatCard = ({
   newMessageAlert,
   index = 0,
 }) => {
-
-  const { handleDeleteChat } = useContext(HandleContext)
+  const { handleDeleteChat } = useContext(HandleContext);
   return (
     <Link
       //   onContextMenu={(e)=>handleDeleteChat(e,_id,groupChat)}
       to={`/chat/${_id}`}
-      className={`w-full rounded-xl px-[1vw] py-[1vh] flex flex-row items-center justify-between ${sameSender ? " bg-[#272727]  " : "  bg-[#272727] bg-opacity-[0.37] "
-        }     hover:bg-opacity-[0.7]  `}
+      className={`w-full rounded-xl px-[1vw] py-[1vh] flex flex-row items-center justify-between ${
+        sameSender ? " bg-[#272727]  " : "  bg-[#272727] bg-opacity-[0.37] "
+      }     hover:bg-opacity-[0.7]  `}
     >
       <div className="flex  items-center gap-5">
         <div className="w-16 h-16 rounded-full  flex relative">
@@ -41,10 +41,15 @@ const ChatCard = ({
           )}
         </div>
         <div className="flex flex-col gap-3">
-          <p className="text-white text-[20px] font-normal leading-5 w-36 overflow-hidden whitespace-nowrap text-ellipsis">{name}</p>
-        { newMessageAlert?.count>1 &&  <p className="text-[#6dff34]  text-[12px] font-normal leading-3 ">
-            {(newMessageAlert?.count)/2 } new {(newMessageAlert?.count)/2 >1 ?"messages":"message"}
-          </p>}
+          <p className="text-white text-[20px] font-normal leading-5 w-36 overflow-hidden whitespace-nowrap text-ellipsis">
+            {name}
+          </p>
+          {newMessageAlert?.count > 0 && (
+            <p className="text-[#6dff34]  text-[12px] font-normal leading-3 ">
+              {newMessageAlert?.count } new{" "}
+              {newMessageAlert?.count > 1 ? "messages" : "message"}
+            </p>
+          )}
         </div>
       </div>
       {/* <div className=" flex flex-col items-end gap-3">
